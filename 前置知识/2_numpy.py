fe8b38ae53babb387ef375f79a80ab947545f78c
Meng <mz_1023@qq.com>
2019-10-02 10:20:32 +0800

    add numpy

---
+++ 前置知识/2_numpy.py
@@ -0,0 +1,14 @@
+# @Time : 2019/10/2 10:07 
+
+# @Author : MZ
+
+# @File : 2_numpy.py 
+
+# @Software: PyCharm
+import numpy as np
+vector=np.array([1,2,3,4])
+print(vector)
+matrix=np.array([[1,'Tim'],[2,'Joey'],[3,'Johnny'],[4,'Frank']])
+print(matrix)
+a=np.arange(15).reshape(3,5)
+print(a)
