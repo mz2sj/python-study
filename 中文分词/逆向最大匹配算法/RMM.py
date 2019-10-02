076f4bf26ac7e765d1105787fe184b5b2f3881dc
Meng <mz_1023@qq.com>
2019-10-02 14:50:15 +0800

    add RMM

--- 中文分词/逆向最大匹配算法/RMM.py
+++ 中文分词/逆向最大匹配算法/RMM.py
@@ -5,3 +5,38 @@
 # @File : RMM.py 
 
 # @Software: PyCharm
+class IMM(object):
+    def __init__(self,dic_path):
+        self.dictionary=set()
+        self.maximum=0
+        # 读取字典
+        with open(dic_path,'r',encoding='utf-8') as f:
+            for line in f:
+                line=line.strip()
+                if not line:
+                    continue
+                self.dictionary.add(line)
+                if(len(line)>self.maximum):
+                    self.maximum=len(line)
+    def cut(self,text):
+        result=[]
+        index=len(text)
+        while index>0:
+            word=None
+            for size in range(self.maximum,0,-1):
+                if index-size<0:
+                    continue
+                piece=text[(index-size):index]
+                if piece in self.dictionary:
+                    word=piece
+                    result.append(word)
+                    index-=size
+                    break
+            if word is None:
+                index-=1
+                result.append('s->'+piece)
+        return result[::-1]
+if __name__ == '__main__':
+    text='南京长江大桥人民解放军'
+    tokenizer=IMM('../../data/imm_dic.utf8')
+    print(tokenizer.cut(text))
